887dc971f6ee231c4753982586400110d9581c91
Andre Soares <andreiarley@gmail.com>
2023-09-04 20:08:34 -0300

    save buddies to local file and add exceptions handler

--- src/presentation/main.py
+++ src/presentation/main.py
@@ -1,36 +1,53 @@
 from ManagerViewModel import ManagerViewModel
 from StudentViewModel import StudentViewModel
 
+class MenuOpcaoInvalida(Exception):
+    "valor da opcao invalida"
+    def __init__(self, message="valor da opcao invalida"):
+        self.message = message
+        super().__init__(self.message)
+
 #mensagem padrao do sistema
 def menu():
-    while True:
-        print("Escolha a opcao:\n[1] Listar buddies.\n[2] Criar buddies.\n[3] Sair.")
+    print("Escolha a opcao:\n[1] Listar buddies.\n[2] Criar buddies.\n[3] Sair.")
+    try: 
         option = int(input())
         if(option in range(1,4)):
             return option
-        print("Opcao invalida\n\n")
+        raise MenuOpcaoInvalida
+    except ValueError:
+        raise MenuOpcaoInvalida
 
 
 def main():
 
     managerVm = ManagerViewModel(id=1, name="Gestor")
-    newId = 0 # Contador de ids 
 
     while True:
-        # Menu de opçoes
-        option = menu()
 
-        if option == 1:
-            # Lista usuarios
-            managerVm.getBuddies()
-        elif option == 2:
-            # Pega informacoes para novo usuario
-            studentVm = StudentViewModel()
-            newStudent = studentVm.createAccount(newId)
-            newId += 1
-            # Salva na ManagerViewModel
-            managerVm.saveBuddy(newStudent)
-        else:
+        try:
+            # Menu de opçoes
+            option = menu()
+
+            if option == 1:
+                # Lista usuarios
+                managerVm.getBuddies()
+            elif option == 2:
+                # Pega informacoes para novo usuario
+                studentVm = StudentViewModel()
+                # incrementa e pega id do ultimo buddy salvo
+                managerVm.incrementLastBuddyId()
+                newStudent = studentVm.createAccount(managerVm.getLastBuddyId())
+                # Salva na ManagerViewModel
+                managerVm.saveBuddy(newStudent)
+            else:
+                print("Encerrando...\n======= Study Buddy Platform =======")
+                break
+        except KeyboardInterrupt:
+            print("\nEncerrando...\n======= Study Buddy Platform =======")
             break
-    
+        except MenuOpcaoInvalida as exc:
+            print(exc.message)
+
+ 
 main()

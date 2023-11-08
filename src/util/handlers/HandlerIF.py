f42b3839168fb3f9611a5345ee6d204de6127135
Calebe Almeida <calebe.almeida@lavid.ufpb.br>
2023-11-08 09:31:55 -0300

    Add Handler abstract class

---
+++ src/util/handlers/HandlerIF.py
@@ -0,0 +1,19 @@
+#classe iterface do handler
+class Handler:
+	def __init__(self) -> None:
+		self.__next = None
+	
+	def setNextHandler(self, next):
+		if self.__next == None:
+			self.__next = next
+		else:
+			self.__next.setNextHandler(next)
+	
+	def handle(self, text: str) -> bool:
+		pass
+
+	def handleNext(self, text: str) -> bool:
+		if self.__next == None:
+			return True
+		return self.__next.handle(text)
+
